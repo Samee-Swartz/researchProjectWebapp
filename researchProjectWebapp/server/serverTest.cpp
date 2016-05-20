4b9757f566b4169445869eccd7733dac533bf1d9
Samee-Swartz <Samee.swartz@gmail.com>
2016-05-20 15:22:29 -0400

    moving stuff around

---
+++ researchProjectWebapp/server/serverTest.cpp
@@ -0,0 +1,16 @@
+#include <boost/python.hpp>
+
+char const* greet()
+{
+   return "hello, world";
+}
+
+BOOST_PYTHON_MODULE(hello_ext)
+{
+    using namespace boost::python;
+    def("greet", greet);
+}
+
+int main() {
+	return 1;
+}
